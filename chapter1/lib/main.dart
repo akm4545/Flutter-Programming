ecab677769d971d6061ecdb87c4e2596468f1dec
akm4545 <akm4545@naver.com>
2025-04-10 16:52:18 +0900

    chapter1

--- chapter1/lib/main.dart
+++ chapter1/lib/main.dart
@@ -187,6 +187,17 @@ void main() {
 
   bool result5 = 12 < 10 || 0 > 1; // 12가 10보다 작거나 0이 1보다 클 때
   print(result5); // false
+
+  int number7 = 2;
+
+  if(number % 3 == 0){
+    print('3의 배수입니다.');
+  }else if(number % 3 == 1){
+    print('나머지가 1입니다.');
+  }else{
+    // 조건에 맞지 않기 떄문에 다음 코드 실행
+    print('맞는 조건이 없습니다.');
+  }
 }
 
 enum Status {
