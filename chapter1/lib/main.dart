fa5192c0e59ce8545515232aa7f5fcf62d1f039e
akm4545 <akm4545@naver.com>
2025-04-10 16:57:15 +0900

    chapter1

--- chapter1/lib/main.dart
+++ chapter1/lib/main.dart
@@ -198,6 +198,25 @@ void main() {
     // 조건에 맞지 않기 떄문에 다음 코드 실행
     print('맞는 조건이 없습니다.');
   }
+
+  switch (status){
+    case Status.approved:
+      // approved 값이기 때문에 다음 코드가 실행된다
+      print('승인 상태입니다.');
+      break;
+    case Status.pending:
+      print('대기 상태입니다.');
+      break;
+    case Status.rejected:
+      print('거절 상태입니다.');
+      break;
+    default:
+      print('알 수 없는 상태입니다.');
+  }
+
+  // Enum의 모든 수를
+  // 리스트로 반환한다
+  print(Status.values);
 }
 
 enum Status {
