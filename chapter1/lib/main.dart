a8692b5e86d0556e3a6b3c152408370222f292c2
akm4545 <akm4545@naver.com>
2025-04-10 17:50:00 +0900

    chapter1

--- chapter1/lib/main.dart
+++ chapter1/lib/main.dart
@@ -292,6 +292,29 @@ void main() {
 
   // 람다 함수로 모든 값 더하기
   final allMembers3 = numbers.reduce((value, element) => value + element);
+
+  void add(int x, int y){
+    print('결괏값 : ${x + y}');
+  }
+
+  void subtract(int x, int y){
+    print('결괏값 : ${x - y}');
+  }
+
+  // typedef는 일반적인 변수의 type처럼 사용 가능
+  Operation oper = add;
+  oper(1, 2);
+
+  // substract() 함수도 Operaion에 해당되는
+  // 시그니처이므로 oper 변수에 저장 가능
+  oper = subtract;
+  oper(1, 2);
+
+  void calculate(int x, int y, Operation oper){
+    oper(x, y);
+  }
+
+  calculate(1, 2, add);
 }
 
 enum Status {
@@ -300,5 +323,6 @@ enum Status {
   rejected
 }
 
+typedef Operation = void Function(int x, int y);
 
 
