76461ba2785b32a6e773dd963a7b013f1de980dd
akm4545 <akm4545@naver.com>
2025-04-15 17:17:07 +0900

    chapter6

--- chapter6/lib/main.dart
+++ chapter6/lib/main.dart
@@ -13,41 +13,41 @@ import 'package:flutter/material.dart';
 //   );
 // }
 
-// void main() {
-//   runApp(
-//     MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: Scaffold(
-//         body: SizedBox(
-//           width: double.infinity,
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//
-//             // 여러 위젯을 Column 위젯에 입력 가능
-//             children: [
-//               Text('Code'),
-//               Text('Factory'),
-//             ],
-//           ),
-//         ),
-//       ),
-//     ),
-//   );
-// }
-
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: , //여기에 예제 코드 작성
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            // 여러 위젯을 Column 위젯에 입력 가능
+            children: [
+              Text('Code'),
+              Text('Factory'),
+            ],
+          ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
+
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: , //여기에 예제 코드 작성
+//         ),
+//       ),
+//     );
+//   }
+// }
