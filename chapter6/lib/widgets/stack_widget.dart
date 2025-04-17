67aa2fc579d8fedd3cad68ecc23dc9e001230567
akm4545 <akm4545@naver.com>
2025-04-17 16:02:21 +0900

    chapter6

---
+++ chapter6/lib/widgets/stack_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class StackWidgetExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Stack(
+            children: [
+              Container(
+                height: 300.0,
+                width: 300.0,
+                color: Colors.red,
+              ),
+              Container(
+                height: 250.0,
+                width: 250.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                height: 200.0,
+                width: 200.0,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
