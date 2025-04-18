8b4fac0fadceb34a59fd72fc7788c2953aa9a12b
akm4545 <akm4545@naver.com>
2025-04-18 11:25:56 +0900

    chapter8

--- chapter8/lib/screen/home_screen.dart
+++ chapter8/lib/screen/home_screen.dart
@@ -12,6 +12,17 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // 앱바 위젯 추가
+      appBar: AppBar(
+        // 배경색 지정
+        backgroundColor: Colors.orange,
+
+        // 앱 타이틀 설정
+        title: Text('Code Factory'),
+
+        // 가운데 정렬
+        centerTitle: true,
+      ),
       body: Text('Home Screen'),
     );
   }   
