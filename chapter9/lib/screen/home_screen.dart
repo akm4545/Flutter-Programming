96f6955a6c50e3225c98378849f162845064dac1
akm4545 <akm4545@naver.com>
2025-04-18 14:40:23 +0900

    chapter9

--- chapter9/lib/screen/home_screen.dart
+++ chapter9/lib/screen/home_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // 상태바 색상 변경
+    // 상태바가 이미 흰색이면 light 대신 dark를 주어 검정으로 바꾸자
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
     return Scaffold(
       // body: Text('Home Screen'),
       body: PageView( // PageView 추가
