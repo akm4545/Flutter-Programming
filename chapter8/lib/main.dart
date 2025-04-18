cc0dc2fdc63c8865c1a768937d590c11dc8704d7
akm4545 <akm4545@naver.com>
2025-04-18 12:00:55 +0900

    chapter8

--- chapter8/lib/main.dart
+++ chapter8/lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  // 플러터 프레임워크가 앱을 실행할 준비가 될 때까지 기다림
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     MaterialApp(
       home: HomeScreen(),
