e3a7da5481b1665a6ecce8d5104bb209ae2b3587
akm4545 <akm4545@naver.com>
2025-04-18 17:35:29 +0900

    chapter10

--- chapter10/lib/screen/home_screen.dart
+++ chapter10/lib/screen/home_screen.dart
@@ -7,7 +7,78 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      backgroundColor: Colors.pink[100], // 핑크 배경색 적용
+      // body: Text('Home Screen'),
+      body: SafeArea( // 시스템 UI 피해서 UI 그리기
+        top: true,
+        bottom: false,
+        child: Column(
+          // 위아래 끝에 위젯 배치
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+          // 반대축 최대 크기로 늘리기
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _DDay(),
+            _CoupleImage()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _DDay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // 테마 불러오기
+    final textTheme = Theme.of(context).textTheme;
+
+    return Column(
+      children: [
+        const SizedBox(height: 16.0),
+        Text( // 최상단 U&I 글자
+          'U&I',
+          style: textTheme.displayLarge, // headline1 스타일 적용
+        ),
+        const SizedBox(height: 16.0),
+        Text( // 두 번째 글자
+          '우리 처음 만난 날',
+          style: textTheme.bodyLarge, // bodyText1 스타일 적용
+        ),
+        Text( // 임시로 지정한 만난 날짜
+          '2021.11.23',
+          style: textTheme.bodyMedium, // bodyText2 스타일 적용
+        ),
+        const SizedBox(height: 16.0),
+        IconButton( // 하트 아이콘 버튼
+          iconSize: 60.0,
+          onPressed: () {},
+          icon: Icon(
+            Icons.favorite,
+            color: Colors.red, // 색상 빨강으로 변경
+          ),
+        ),
+        const SizedBox(height: 16.0),
+        Text( // 만난 후 DDay
+          'D+365',
+          style: textTheme.displayMedium, // headline2 스타일 적용
+        ),
+      ],
+    );
+  }
+}
+
+class _CoupleImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center( // 이미지 중앙 정렬
+      child: Image.asset(
+        'asset/img/middle_image.png',
+
+        // 화면의 반만큼 높이 구현
+        height: MediaQuery.of(context).size.height / 2,
+      ),
     );
   }
 }
