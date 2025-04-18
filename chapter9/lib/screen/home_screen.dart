b7baba40897ff4f950e7783eecb9c64ee02c461b
akm4545 <akm4545@naver.com>
2025-04-18 14:34:13 +0900

    chapter9

--- chapter9/lib/screen/home_screen.dart
+++ chapter9/lib/screen/home_screen.dart
@@ -11,7 +11,10 @@ class HomeScreen extends StatelessWidget {
       body: PageView( // PageView 추가
         children: [1, 2, 3, 4, 5] //샘플 리스트 생성
         .map( // 위젯으로 매핑
-            (number) => Image.asset('asset/img/image_$number.jpeg'),
+            (number) => Image.asset(
+                'asset/img/image_$number.jpeg',
+                fit: BoxFit.cover, // BoxFit.cover 설정
+            ),
         ).toList(),
       ),
     );
