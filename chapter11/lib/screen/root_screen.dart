277ce6215e6f1da2e7d0d2a7ab0c0c4eb4b5e85c
akm4545 <akm4545@naver.com>
2025-04-21 15:58:50 +0900

    chapter11

--- chapter11/lib/screen/root_screen.dart
+++ chapter11/lib/screen/root_screen.dart
@@ -1,7 +1,9 @@
+import 'dart:math';
+
 import 'package:chapter11/screen/home_screen.dart';
 import 'package:chapter11/screen/settings_screen.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shake/shake.dart';
 
 // class RootScreen extends StatelessWidget {
 class RootScreen extends StatefulWidget {
@@ -15,6 +17,8 @@ class _RootScreenState extends State<RootScreen> with
 TickerProviderStateMixin{ // TickerProviderStateMixin 사용하기
   TabController? controller; // 사용할 TabController 선언
   double threshold = 2.7; // 민감도의 기본값 설정
+  int number = 1; // 주사위 숫자
+  ShakeDetector? shakeDetector;
 
   @override
   void initState() {
@@ -25,18 +29,34 @@ TickerProviderStateMixin{ // TickerProviderStateMixin 사용하기
     // 컨트롤러 속성이 변경될 때마다 실행할 함수 등록
     // ! -> nullable 타입일 가능성을 해당 시점에선 절대 null이 아님을 알려주는 연산자
     controller!.addListener(tabListener);
+
+    print("자이로 등록");
+    
+    shakeDetector = ShakeDetector.autoStart( // 흔들기 감지 즉시 시작
+      shakeSlopTimeMS: 100, // 감지 주기
+      shakeThresholdGravity: threshold, // 감지 민감도
+      onPhoneShake: onPhoneShake, // 감지 후 실행할 함수
+    );
+  }
+
+  void onPhoneShake() { // 감지 후 실행할 함수
+    print("감지!!");
+    final rand = new Random();
+
+    setState(() {
+      number = rand.nextInt(5) + 1;
+    });
   }
 
   // 리스너로 사용할 함수
   tabListener() {
-    setState(() {
-
-    });
+    setState(() {});
   }
 
   @override
   void dispose() {
     controller!.removeListener(tabListener); // 리스너에 등록한 함수 등록 취소
+    shakeDetector!.stopListening(); // 흔들기 감지 중지
     super.dispose();
   }
 
@@ -56,7 +76,8 @@ TickerProviderStateMixin{ // TickerProviderStateMixin 사용하기
   List<Widget> renderChildren() {
     return [
       // HomeScreen을 불러와서 입력하기
-      HomeScreen(number: 1),
+      // HomeScreen(number: 1),
+      HomeScreen(number: number), // number 변수로 대체
       SettingScreen( // 기존에 있던 Container 코드를 통째로 교체
           threshold: threshold,
           onThresholdChange: onThresholdChange
