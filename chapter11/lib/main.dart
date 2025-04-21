98c6eac86e03320251415ab31f3bcc09a6ae21a8
akm4545 <akm4545@naver.com>
2025-04-21 11:43:47 +0900

    chapter11

--- chapter11/lib/main.dart
+++ chapter11/lib/main.dart
@@ -1,9 +1,27 @@
+import 'package:chapter11/const/colors.dart';
 import 'package:chapter11/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: backgroundColor,
+        sliderTheme: SliderThemeData( // Slider 위젯 관련 테마
+          thumbColor: primaryColor, // 노브 색상
+          activeTrackColor: primaryColor, // 노브가 이동한 트랙 색상
+
+          // 노브가 아직 이동하지 않은 트랙 색상
+          inactiveTrackColor: primaryColor.withOpacity(0.3);
+        ),
+
+        // BottomNavigationBar 위젯 관련 테마
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          selectedItemColor: primaryColor, // 선택 상태 색
+          unselectedItemColor: secondaryColor, // 비선택 상태 색
+          backgroundColor: backgroundColor, // 배경색
+        ),
+      ),
       home: HomeScreen(),
     ),
   );
