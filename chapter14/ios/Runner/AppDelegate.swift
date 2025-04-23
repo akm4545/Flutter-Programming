faa9dd1abf2e9d907ecf775b40957efd43be54bf
akm4545 <akm4545@naver.com>
2025-04-23 14:15:51 +0900

    chapter14

--- chapter14/ios/Runner/AppDelegate.swift
+++ chapter14/ios/Runner/AppDelegate.swift
@@ -1,13 +1,30 @@
-import Flutter
-import UIKit
+// import Flutter
+// import UIKit
+//
+// @main
+// @objc class AppDelegate: FlutterAppDelegate {
+//   override func application(
+//     _ application: UIApplication,
+//     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//   ) -> Bool {
+//     GeneratedPluginRegistrant.register(with: self)
+//     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//   }
+// }
 
-@main
+import UIKit
+import Flutter
+import GoogleMaps
+
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions:
+    [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GMSServices.provideAPIKey("API_KEY")
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
