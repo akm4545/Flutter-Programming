e026e1f26ac5183a12d63353e728ba602204cd3c
akm4545 <akm4545@naver.com>
2025-04-23 18:23:16 +0900

    chapter15

--- chapter15/lib/screen/home_screen.dart
+++ chapter15/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chapter15/component/main_app_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +8,29 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      // body: Text('Home Screen'),
+      body: Stack( // 스크린에 Body, AppBar, Footer 순서로 쌓을 준비
+        fit: StackFit.expand, // 자식 위젯들 최대 크기로 펼치기
+        children: [
+          // MainAppBar를 좌, 우, 위 끝에 정렬
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: MainAppBar(
+              onPickImage: onPickImage,
+              onSaveImage: onSaveImage,
+              onDeleteItem: onDeleteItem,
+            ),
+          ),
+        ],
+      ),
     );
   }
+
+  void onPickImage(){}
+
+  void onSaveImage(){}
+
+  void onDeleteItem(){}
 }
