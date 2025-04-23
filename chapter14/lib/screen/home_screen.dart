1ade0abb5bb0d5330903cffd4c46c123395b2339
akm4545 <akm4545@naver.com>
2025-04-23 15:47:23 +0900

    chapter14

--- chapter14/lib/screen/home_screen.dart
+++ chapter14/lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -14,35 +15,57 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold( // 지도 위치 지정
       appBar: renderAppBar(),
-      body: Column(
-        children: [
-          Expanded( // 2/3만큼 공간 차지
-            flex: 2,
-            child: GoogleMap(
-              initialCameraPosition: CameraPosition(
-                target: companyLatLng,
-                zoom: 16, // 확대 정도 (높을수록 크게 보임)
+      body: FutureBuilder<String>(
+          future: checkPermission(),
+          builder: (context, snapshot){
+            // 로딩 상태
+            if(!snapshot.hasData && snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            // 권한 허가된 상태
+            if(snapshot.data == '위치 권한이 허가 되었습니다.'){
+              return Column(
+                children: [
+                  Expanded( // 2/3만큼 공간 차지
+                    flex: 2,
+                    child: GoogleMap(
+                      initialCameraPosition: CameraPosition(
+                        target: companyLatLng,
+                        zoom: 16, // 확대 정도 (높을수록 크게 보임)
+                      ),
+                    ),
+                  ),
+                  Expanded( // 1/3 만큼 공간 차지
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon( // 시계 아이콘
+                          Icons.timelapse_outlined,
+                          color: Colors.blue,
+                          size: 50.0,
+                        ),
+                        const SizedBox(height: 20.0),
+                        ElevatedButton( // [출근하기] 버튼
+                          onPressed: () {},
+                          child: Text('출근하기!'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            }
+
+            // 권한 없는 상태
+            return Center(
+              child: Text(
+                snapshot.data.toString(),
               ),
-            ),
-          ),
-          Expanded( // 1/3 만큼 공간 차지
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon( // 시계 아이콘
-                  Icons.timelapse_outlined,
-                  color: Colors.blue,
-                  size: 50.0,
-                ),
-                const SizedBox(height: 20.0),
-                ElevatedButton( // [출근하기] 버튼
-                    onPressed: () {},
-                    child: Text('출근하기!'),
-                ),
-              ],
-            ),
-          ),
-        ],
+            );
+          }
       ),
     );
   }
@@ -60,5 +83,35 @@ class HomeScreen extends StatelessWidget {
       ),
       backgroundColor: Colors.white,
     );
+  }
+
+  Future<String> checkPermission() async {
+    // 위치 서비스 활성화 여부 확인
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    // 위치 서비스 활성화 여부 확인
+    if(!isLocationEnabled) { // 위치 서비스 활성화 안 됨
+      return '위치 서비스를 활성화해주세요.';
+    }
+
+    // 위치 권한 확인
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+
+    if(checkedPermission == LocationPermission.denied) { // 위치 권한 거절됨
+      // 위치 권한 요청하기
+      checkedPermission = await Geolocator.requestPermission();
+
+      if (checkedPermission == LocationPermission.denied) {
+        return '위치 권한을 허가해주세요.';
+      }
+    }
+
+    // 위치 권한 거절됨 (앱에서 재요청 불가)
+    if(checkedPermission == LocationPermission.deniedForever){
+      return '앱의 위치 권한을 설정에서 허가해주세요.';
+    }
+
+    // 위 모든 조건이 통과되면 위치 권한 허가 완료
+    return '위치 권한이 허가 되었습니다.';
   }
 }
