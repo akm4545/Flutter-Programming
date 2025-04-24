434bcba5a292350e4561860bbf013404d3140626
akm4545 <akm4545@naver.com>
2025-04-24 14:41:35 +0900

    chapter15

--- chapter15/lib/screen/home_screen.dart
+++ chapter15/lib/screen/home_screen.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 
+import 'package:chapter15/component/emoticon_sticker.dart';
 import 'package:chapter15/component/footer.dart';
 import 'package:chapter15/component/main_app_bar.dart';
+import 'package:chapter15/model/sticker_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
 
 // class HomeScreen extends StatelessWidget {
 class HomeScreen extends StatefulWidget {
@@ -16,6 +19,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen>{
   XFile? image; // 선택한 이미지를 저장할 변수
+  Set<StickerModel> stickers = {}; // 화면에 추가된 스티커를 저장할 변수
+  String? selectedId; // 현재 선택된 스티커의 ID
 
   @override
   Widget build(BuildContext context) {
@@ -58,11 +63,27 @@ class _HomeScreenState extends State<HomeScreen>{
       return Positioned.fill(
         // 위젯 확대 및 좌우 이동을 가능하게 하는 위젯
         child: InteractiveViewer(
-          child: Image.file(
-            File(image!.path),
-
-            // 이미지가 부모 위젯 크기 최대를 차지하도록 하기
-            fit: BoxFit.cover,
+          child: Stack(
+            fit: StackFit.expand, // 크기 최대로 늘려주기
+            children: [
+              Image.file(
+                File(image!.path),
+                fit: BoxFit.cover, // 이미지 최대한 공간 차지하게 하기
+              ),
+              ...stickers.map(
+                (sticker) => Center( // 최초 스티커 선택 시 중앙에 배치
+                  child: EmoticonSticker(
+                    key: ObjectKey(sticker.id),
+                    onTransform: () {
+                      // 스티커의 ID값 함수의 매개변수로 전달
+                      onTransform(sticker.id);
+                    },
+                    imgPath: sticker.imgPath,
+                    isSelected: selectedId == sticker.id,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       );
@@ -80,7 +101,25 @@ class _HomeScreenState extends State<HomeScreen>{
     }
   }
 
-  void onEmotionTap(int index){}
+  void onTransform(String id) {
+    // 스티커가 변형될 때마다 변형 중인
+    // 스티커를 현재 선택한 스티커로 지정
+    setState(() {
+      selectedId = id;
+    });
+  }
+
+  void onEmotionTap(int index){
+    setState(() {
+      stickers = {
+        ...stickers,
+        StickerModel(
+          id: Uuid().v4(), // 스티커의 고유 ID
+          imgPath: 'asset/img/emoticon_$index.png',
+        ),
+      };
+    });
+  }
 
   void onPickImage() async { 
     final image = await ImagePicker().pickImage(source: ImageSource.gallery);
