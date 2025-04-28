11b2b5b7becf109459b8a28d84a3ac3edd8f3f4c
akm4545 <akm4545@naver.com>
2025-04-28 14:24:44 +0900

    chapter17

--- chapter17/lib/screen/home_screen.dart
+++ chapter17/lib/screen/home_screen.dart
@@ -1,13 +1,44 @@
+import 'package:chapter17/component/main_calendar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+// class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc( // 선택된 날짜를 관리할 변수
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      body: SafeArea( // 시스템 UI 피해서 UI 구현하기
+        child: Column( // 달력과 리스트를 세로로 배치
+          children: [
+            // 미리 작업해둔 달력 위젯 보여주기
+            MainCalendar(
+              selectedDate: selectedDate, // 선택된 날짜 전달하기
+              onDaySelected: onDaySelected, // 날짜가 선택됐을 때 실행할 함수
+            ),
+          ],
+        ),
+      ),
+      // body: Text('Home Screen'),
     );
+  }
+
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate){
+    // 날짜 선택될 때마다 실행할 함수
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
   }
 }
