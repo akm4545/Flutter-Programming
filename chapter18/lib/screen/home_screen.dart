2000b4c3c22245cd747c2e743d5548ed74d19a9b
akm4545 <akm4545@naver.com>
2025-04-30 14:14:49 +0900

    chapter18

---
+++ chapter18/lib/screen/home_screen.dart
@@ -0,0 +1,74 @@
+import 'package:chapter17/component/main_calendar.dart';
+import 'package:chapter17/component/schedule_bottom_sheet.dart';
+import 'package:chapter17/component/schedule_card.dart';
+import 'package:chapter17/component/today_banner.dart';
+import 'package:chapter17/const/colors.dart';
+import 'package:flutter/material.dart';
+
+// class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc( // 선택된 날짜를 관리할 변수
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton( // 새 일정 버튼
+        backgroundColor: PRIMARY_COLOR,
+        onPressed: () {
+          showModalBottomSheet( // BottomSheet 열기
+            context: context,
+            isDismissible: true, // 배경 탭했을 때 BottomSheet 닫기
+            builder: (_) => ScheduleBottomSheet(),
+            // BottomSheet의 높이를 화면의 최대 높이로
+            // 정의하고 스크롤 가능하게 변경
+            isScrollControlled: true,
+          );
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+      body: SafeArea( // 시스템 UI 피해서 UI 구현하기
+        child: Column( // 달력과 리스트를 세로로 배치
+          children: [
+            // 미리 작업해둔 달력 위젯 보여주기
+            MainCalendar(
+              selectedDate: selectedDate, // 선택된 날짜 전달하기
+              onDaySelected: onDaySelected, // 날짜가 선택됐을 때 실행할 함수
+            ),
+            SizedBox(height: 8.0),
+            TodayBanner( // 배너 추가하기
+              selectedDate: selectedDate,
+              count: 0,
+            ),
+            SizedBox(height: 8.0),
+            ScheduleCard( // 구현해둔 일정 카드
+              startTime: 12,
+              endTime: 14,
+              content: '프로그래밍 공부',
+            ),
+          ],
+        ),
+      ),
+      // body: Text('Home Screen'),
+    );
+  }
+
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate){
+    // 날짜 선택될 때마다 실행할 함수
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
+  }
+}
