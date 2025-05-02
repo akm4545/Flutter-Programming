535e82827446e2696f02e504f3bbda0d9eaddb43
akm4545 <akm4545@naver.com>
2025-05-02 12:27:21 +0900

    chapter19

--- chapter19/lib/main.dart
+++ chapter19/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:chapter19/database/drift_database.dart';
+import 'package:chapter19/provider/schedule_provider.dart';
+import 'package:chapter19/repository/schedule_repository.dart';
 import 'package:chapter19/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:get_it/get_it.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   // 플러터 프레임워크가 준비될 때까지 대기
@@ -16,9 +19,15 @@ void main() async {
   // GetIt에 데이터베이스 변수 주입하기
   GetIt.I.registerSingleton<LocalDatabase>(database);
 
+  final repository = ScheduleRepository();
+  final scheduleProvider = ScheduleProvider(repository: repository);
+
   runApp(
-    MaterialApp(
-      home: HomeScreen(),
-    )
+    ChangeNotifierProvider( // Provider 하위 위젯에 제공하기
+      create: (_) => scheduleProvider,
+      child: MaterialApp(
+        home: HomeScreen(),
+      ),
+    ),
   );
 }
