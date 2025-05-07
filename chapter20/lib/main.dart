04bd40c31e64902ff7ad552ba41c029a0c564f51
akm4545 <akm4545@naver.com>
2025-05-07 15:35:13 +0900

    chapter20

--- chapter20/lib/main.dart
+++ chapter20/lib/main.dart
@@ -2,14 +2,22 @@ import 'package:chapter20/database/drift_database.dart';
 import 'package:chapter20/provider/schedule_provider.dart';
 import 'package:chapter20/repository/schedule_repository.dart';
 import 'package:chapter20/screen/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
+import 'package:chapter20/firebase_options.dart';
+
 
 void main() async {
   // 플러터 프레임워크가 준비될 때까지 대기
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 파이어베이스 프로젝트 설정 함수
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await initializeDateFormatting(); //initl 패키지 초기화 (다국어화)
 
