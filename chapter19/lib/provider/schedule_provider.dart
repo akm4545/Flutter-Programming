440923ae401682db405bfc5552d2f3cefc606d76
akm4545 <akm4545@naver.com>
2025-05-07 11:08:34 +0900

    chapter19

--- chapter19/lib/provider/schedule_provider.dart
+++ chapter19/lib/provider/schedule_provider.dart
@@ -1,6 +1,7 @@
 import 'package:chapter19/repository/schedule_repository.dart';
 import 'package:chapter19/model/schedule_model.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 
 class ScheduleProvider extends ChangeNotifier {
@@ -37,41 +38,98 @@ class ScheduleProvider extends ChangeNotifier {
   }) async {
     final targetDate = schedule.date;
 
-    final savedSchedule = await repository.createSchedule(schedule: schedule);
+    final uuid = Uuid();
 
-    cache.update(
-      targetDate,
-      (value) => [ // 현존하는 캐시 리스트 끝에 새로운 일정 추가
-        ...value,
-        schedule.copyWith(
-          id: savedSchedule, // ID 값은 저장해야 생성되니 입력한 객체 복사 후 추가
-        ),
-      ]..sort(
-        (a, b) =>
-          a.startTime.compareTo(
-            b.startTime,
-          ),
-      ),
-      // 날짜에 해당되는 값이 없다면 새로운 리스트에 새로운 일정 하나만 추가
-      ifAbsent: () => [schedule],
+    final tempId = uuid.v4(); // 유일한 ID값 생성
+    final newSchedule = schedule.copyWith(
+      id: tempId, // 임시 ID를 지정
     );
 
+    // 긍정적 응답 구간. 서버에서 응답을 받기 전에 캐시를 먼저 업데이트한다
+    cache.update(
+      targetDate,
+          (value) => [ // 현존하는 캐시 리스트 끝에 새로운 일정 추가
+        ...value,
+        // schedule.copyWith(
+        //   id: savedSchedule, // ID 값은 저장해야 생성되니 입력한 객체 복사 후 추가
+        // ),
+        newSchedule,    
+      ]..sort(
+            (a, b) =>
+            a.startTime.compareTo(
+              b.startTime,
+            ),
+      ),
+      // 날짜에 해당되는 값이 없다면 새로운 리스트에 새로운 일정 하나만 추가
+      ifAbsent: () => [newSchedule],
+    );
+
+    // 캐시 업데이트 반영
     notifyListeners();
+
+    try{
+      // API 요청을 한다
+      final savedSchedule = await repository.createSchedule(schedule: schedule);
+
+      // 서버 응답 기반으로 캐시 업데이트
+      cache.update(
+        targetDate,
+        (value) => value
+            .map((e) => e.id == tempId
+              ? e.copyWith(
+                id: savedSchedule,
+              )
+            : e)
+            .toList(),
+      );
+    }catch(e){
+      // 생성 실패 시 캐시 롤백
+      cache.update(
+        targetDate,
+        (value) => value.where((e) => e.id != tempId).toList(),
+      );
+
+      notifyListeners();
+    }
   }
 
   void deleteSchedule({
     required DateTime date,
     required String id,
   }) async {
-    final resp = await repository.deleteSchedule(id: id);
-
-    cache.update( // 캐시에서 데이터 삭제
-      date,
-      (value) => value.where((e) => e.id != id).toList(),
-                 ifAbsent: () => [],
+    // 삭제할 일정 기억
+    final targetSchedule = cache[date]!.firstWhere(
+          (e) => e.id == id,
     );
 
+    // 긍정적 응답 (응답 전에 캐시 먼저 업데이트)
+    cache.update( // 캐시에서 데이터 삭제
+      date,
+          (value) => value.where((e) => e.id != id).toList(),
+      ifAbsent: () => [],
+    );
+
+    // 캐시 업데이트 반영하기
     notifyListeners();
+
+    try{
+      // 삭제 함수 실행
+      await repository.deleteSchedule(id: id);
+    }catch(e){
+      // 삭제 실패 시 캐시 롤백하기
+      cache.update(
+        date,
+        (value) => [...value, targetSchedule]..sort(
+          (a, b) => a.startTime.compareTo(
+            b.startTime,
+          ),
+        ),
+      );
+
+      notifyListeners();
+    }
+
+    // final resp = await repository.deleteSchedule(id: id);
   }
 
   void changeSelectedDate({
--- chapter19/lib/repository/schedule_repository.dart
+++ chapter19/lib/repository/schedule_repository.dart
@@ -6,7 +6,8 @@ import 'package:dio/dio.dart';
 class ScheduleRepository {
   final _dio = Dio();
   // 안드로이드에서는 10.0.2.2가 localhost에 해당함
-  final _targetUrl = 'http://${Platform.isAndroid ? '10.0.2.2' : 'localhost'}:3000/schedule';
+  // final _targetUrl = 'http://${Platform.isAndroid ? '10.0.2.2' : 'localhost'}:3000/schedule';
+  final _targetUrl = 'http://${Platform.isAndroid ? '192.168.10.119' : 'localhost'}:3000/schedule';
 
   Future<List<ScheduleModel>> getSchedules({
     required DateTime date,
