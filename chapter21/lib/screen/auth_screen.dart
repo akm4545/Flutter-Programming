1d24a2a9496debf8b1697de5838475aa60752d67
akm4545 <akm4545@naver.com>
2025-05-08 18:59:33 +0900

    chapter21

---
+++ chapter21/lib/screen/auth_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  // @override
+  // Widget build(BuildContext context) {
+  //   return const Placeholder();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // 깔끔한 디자인을 위해 좌, 우로 패딩을 준다
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          // 세로로 중앙 배치한다
+          mainAxisAlignment: MainAxisAlignment.center,
+          // 가로로 최대한의 크기로 늘려준다
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // 로고를 화면 너비의 절반만큼의 크기로 렌더링하고 가운데 정렬한다
+            Align(
+              alignment: Alignment.center,
+              child: ,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
