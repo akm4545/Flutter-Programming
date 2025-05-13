143cdef7307d72a61f8d846f3433e8e3518531ff
akm4545 <akm4545@naver.com>
2025-05-13 17:31:52 +0900

    chapter22

--- chapter22/lib/screen/auth_screen.dart
+++ chapter22/lib/screen/auth_screen.dart
@@ -1,6 +1,7 @@
 import 'package:chapter22/const/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthScreen extends StatelessWidget {
   const AuthScreen({Key? key}) : super(key: key);
@@ -27,7 +28,8 @@ class AuthScreen extends StatelessWidget {
 
             // 구글로 로그인 버튼
             ElevatedButton(
-              onPressed: () {},
+              // 나중에 BuildContext가 필요하기 때문에 전달해준다
+              onPressed: () => onGoogleLoginPress(context),
               style: ElevatedButton.styleFrom(
                 foregroundColor: Colors.white,
                 backgroundColor: SECONDARY_COLOR,
@@ -41,5 +43,33 @@ class AuthScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  onGoogleLoginPress(BuildContext context) async {
+    // 구글 로그인 객체를 생성
+    GoogleSignIn googleSignIn = GoogleSignIn(
+      scopes: [
+        'email',
+      ],
+    );
+
+    try{
+      // signIn 함수를 실행해서 로그인을 진행한다
+      GoogleSignInAccount? account = await googleSignIn.signIn();
+
+      // 어떤 값을 반환받는지 출력하여 확인한다
+      print(account);
+    }catch(error){
+      print("==============================");
+      print("==============================");
+      print("==============================");
+      print("==============================");
+      print("==============================");
+      print(error);
+      // 로그인 에러가 나면 Snackbar를 보여준다
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('로그인 실패')),
+      );
+    }
   }
 }
