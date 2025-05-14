d51cd7e4afed5488b1fa71955d2e9fa46c3281f6
akm4545 <akm4545@naver.com>
2025-05-14 12:44:25 +0900

    chapter23

--- chapter23/lib/main.dart
+++ chapter23/lib/main.dart
@@ -9,16 +9,22 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 import 'package:chapter23/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 
 void main() async {
   // 플러터 프레임워크가 준비될 때까지 대기
   WidgetsFlutterBinding.ensureInitialized();
 
-  // 파이어베이스 프로젝트 설정 함수
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  await Supabase.initialize(
+    url: '{Project URL 입력}',
+    anonKey: '{API Key 입력}',
   );
+
+  // 파이어베이스 프로젝트 설정 함수
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
 
   await initializeDateFormatting(); //initl 패키지 초기화 (다국어화)
 
